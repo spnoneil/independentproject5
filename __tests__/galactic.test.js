bcab3a4070763e5b99421b6d0d060935ada63b73
Scott O'Neil <scottieoneil@gmail.com>
2021-02-05 08:19:20 -0800

    add earth age test; bad fail

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,30 +1,10 @@
-import Template from './../src/template.js';
+describe('Age', () => {
+  let age;
+})
 
-
-describe('Template', () => {
-  let reusableTemplate;
-
-  beforeEach(() => {
-    reusableTemplate = new Template(5, 5, 5);
-  });
-
-  test('should show how beforeEach() works', () => {
-    console.log(reusableTemplate);
-  });
-});
-
-// import Example from '../src/example.js';
-
-// describe('Example', () => {
-
-//   test('should correctly demonstrate bad fails', () => {
-//     let example = new Example();
-//     expect(example.data).toEqual("Bad fail!");
-//   });
-
-//   test('should correctly demonstrate bad fails', () => {
-//     let example = new Example();
-//     expect(example.exampleFunction()).toEqual("This function returns a bad fail!");
-//   });
-
-// });
+beforeEach(() => {
+  age = new Age(34);
+})
+test('should create an age object', () => {
+  expect(age.earth).toEqual(34);
+})
