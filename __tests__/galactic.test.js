e94047523be939d4e2352ffc68725fd65512757b
Scott O'Neil <scottieoneil@gmail.com>
2021-02-05 08:23:42 -0800

    run earth test; connected properly, fail

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -1,10 +1,13 @@
+import Age from './../src/galactic.js'
+
 describe('Age', () => {
   let age;
-})
 
-beforeEach(() => {
-  age = new Age(34);
-})
-test('should create an age object', () => {
-  expect(age.earth).toEqual(34);
-})
+
+  beforeEach(() => {
+    age = new Age(34);
+  });
+  test('should create an age object', () => {
+    expect(age.earth).toEqual(34);
+  });
+});
