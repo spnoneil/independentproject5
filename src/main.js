2b8f6b917de62e627bbba63f43cb752bec11a28e
Scott O'Neil <scottieoneil@gmail.com>
2021-02-05 14:15:30 -0800

    start UI logic

--- src/main.js
+++ src/main.js
@@ -2,3 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { Age, pastExpectancy } from './galactic.js';
+
+$(document).ready(function() {
+  $('#form').submit(function(e) {
+    e.preventDefault;
+    const userName = $("#userInput").val();
+    const userAge = $("#nameInput").val();
+    console.log(userName);
+    console.log(userAge);
+    let age = new Age(userAge);
+    age.calculateAge();
+    age.lifeExpectancy();
+    age.yearsLeft();
+    pastExpectancy(age);
+    console.log(age);
+  });
+});
