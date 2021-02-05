db5aabc719a8a1c8ab22541759354e26909f0ea8
Scott O'Neil <scottieoneil@gmail.com>
2021-02-05 15:12:27 -0800

    continue ui

--- src/main.js
+++ src/main.js
@@ -5,17 +5,25 @@ import './css/styles.css';
 import { Age, pastExpectancy } from './galactic.js';
 
 $(document).ready(function() {
-  $('#form').submit(function(e) {
-    e.preventDefault;
-    const userName = $("#userInput").val();
-    const userAge = $("#nameInput").val();
-    console.log(userName);
-    console.log(userAge);
+  $('form').submit(function(e) {
+    e.preventDefault();
+    const userName = $("#nameInput").val();
+    const userAge = parseInt($("#ageInput").val());
     let age = new Age(userAge);
+    let expectantCheck = pastExpectancy(age)
     age.calculateAge();
     age.lifeExpectancy();
     age.yearsLeft();
-    pastExpectancy(age);
+    console.log(pastExpectancy(age))
     console.log(age);
+    $("#hide").show();
+    $("#nameOutput").text(userName);
+    if (pastExpectancy(age) = []) {
+      $("#earthOutput").append("<li>" + `On Earth, you're ${age.earthAge} years old, with an average life expectancy of ${age.earthExpectancy}. This means you have ${age.earthYearsLeft} years left! Congratulations?` + "</li>");
+      $("#mercuryOutput").append("<li>" + `On Mercury, you're ${age.mercuryAge} years old, with an average life expectancy of ${age.mercuryExpectancy}. This means you have ${age.mercuryYearsLeft} years left! Congratulations?` + "</li>");
+      $("#venusOutput").append("<li>" + `On Venus, you're ${age.venusAge} years old, with an average life expectancy of ${age.venusExpectancy}. This means you have ${age.venusYearsLeft} years left! Congratulations?` + "</li>");
+      $("#marsOutput").append("<li>" + `On Mars, you're ${age.marsAge} years old, with an average life expectancy of ${age.marsExpectancy}. This means you have ${age.marsYearsLeft} years left! Congratulations?` + "</li>");
+      $("#jupiterOutput").append("<li>" + `On Jupiter, you're ${age.jupiterAge} years old, with an average life expectancy of ${age.jupiterExpectancy}. This means you have ${age.jupiterYearsLeft} years left! Congratulations?` + "</li>");
+    }
   });
 });
