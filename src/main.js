5a87d081052c6bf5e2c79eba3a9a9dabdb31d76e
Scott O'Neil <scottieoneil@gmail.com>
2021-02-05 16:17:13 -0800

    update ui

--- src/main.js
+++ src/main.js
@@ -7,18 +7,25 @@ import { Age, pastExpectancy } from './galactic.js';
 $(document).ready(function() {
   $('form').submit(function(e) {
     e.preventDefault();
+    $('ul').empty();
     const userName = $("#nameInput").val();
     const userAge = parseInt($("#ageInput").val());
     let age = new Age(userAge);
-    let expectantCheck = pastExpectancy(age)
     age.calculateAge();
     age.lifeExpectancy();
     age.yearsLeft();
-    console.log(pastExpectancy(age))
+    pastExpectancy(age);
     console.log(age);
     $("#hide").show();
     $("#nameOutput").text(userName);
-    if (pastExpectancy(age) = []) {
+    console.log(age.earthYearsLeft);
+    if (age.earthYearsLeft < 0) {
+      $("#earthOutput").append("<li>" + `On Earth, you're ${age.earthAge} years old, with an average life expectancy of ${age.earthExpectancy}. This means you have exceeded the limit by ${age.earthYearsLeft} years! Congratulations!` + "</li>");
+      $("#mercuryOutput").append("<li>" + `On Mercury, you're ${age.mercuryAge} years old, with an average life expectancy of ${age.mercuryExpectancy}. This means you have exceeded the limit by ${age.mercuryYearsLeft} years! Congratulations!` + "</li>");
+      $("#venusOutput").append("<li>" + `On Venus, you're ${age.venusAge} years old, with an average life expectancy of ${age.venusExpectancy}. This means you have exceeded the limit by ${age.venusYearsLeft} years! Congratulations!` + "</li>");
+      $("#marsOutput").append("<li>" + `On Mars, you're ${age.marsAge} years old, with an average life expectancy of ${age.marsExpectancy}. This means you have exceeded the limit by ${age.marsYearsLeft} years! Congratulations!` + "</li>");
+      $("#jupiterOutput").append("<li>" + `On Jupiter, you're ${age.jupiterAge} years old, with an average life expectancy of ${age.jupiterExpectancy}. This means you have exceeded the limit by ${age.jupiterYearsLeft} years! Congratulations?` + "</li>");
+    } else {
       $("#earthOutput").append("<li>" + `On Earth, you're ${age.earthAge} years old, with an average life expectancy of ${age.earthExpectancy}. This means you have ${age.earthYearsLeft} years left! Congratulations?` + "</li>");
       $("#mercuryOutput").append("<li>" + `On Mercury, you're ${age.mercuryAge} years old, with an average life expectancy of ${age.mercuryExpectancy}. This means you have ${age.mercuryYearsLeft} years left! Congratulations?` + "</li>");
       $("#venusOutput").append("<li>" + `On Venus, you're ${age.venusAge} years old, with an average life expectancy of ${age.venusExpectancy}. This means you have ${age.venusYearsLeft} years left! Congratulations?` + "</li>");
